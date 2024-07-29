0e6da5caa85b44cf46687cbf0ea93a8b4b590d21
Daniel Filipowicz <daniel04f@gmail.com>
2024-07-29 22:14:22 +0200

    -Added dodajUbranie method -Added getUbrania method -Added Iterable implementation and iterator method

--- src/main/java/pl/kurs/test4/task1/Szafa.java
+++ src/main/java/pl/kurs/test4/task1/Szafa.java
@@ -1,12 +1,27 @@
 package pl.kurs.test4.task1;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
-public class Szafa {
+public class Szafa implements Iterable<Ubranie> {
     private List<Ubranie> ubrania;
 
     public Szafa() {
         this.ubrania = new ArrayList<>();
+    }
+
+    public void dodajUbranie(Ubranie... ubrania) {
+        Collections.addAll(this.ubrania, ubrania);
+    }
+
+    public List<Ubranie> getUbrania() {
+        return new ArrayList<>(ubrania);
+    }
+
+    @Override
+    public Iterator<Ubranie> iterator() {
+        return ubrania.iterator();
     }
 }
