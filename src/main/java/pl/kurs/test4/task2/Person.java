5a61856235f7de45ca5e7422a7fbdde9deac915c
Daniel Filipowicz <daniel04f@gmail.com>
2024-07-29 22:24:13 +0200

    -Added Constructor -Added Getters -Added isWoman method

--- src/main/java/pl/kurs/test4/task2/Person.java
+++ src/main/java/pl/kurs/test4/task2/Person.java
@@ -5,4 +5,23 @@ public class Person {
     private String lastName;
     private String city;
     private int age;
+
+    public Person(String firstName, String lastName, String city, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.city = city;
+        this.age = age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean isWoman() {
+        return firstName.endsWith("a");
+    }
 }
