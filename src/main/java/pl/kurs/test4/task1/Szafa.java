6a58a4cab763e2392efda61260e3a35ac28c5913
Daniel Filipowicz <daniel04f@gmail.com>
2024-07-29 22:13:36 +0200

    -Added class field -Added Constructor

--- src/main/java/pl/kurs/test4/task1/Szafa.java
+++ src/main/java/pl/kurs/test4/task1/Szafa.java
@@ -1,4 +1,12 @@
 package pl.kurs.test4.task1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Szafa {
+    private List<Ubranie> ubrania;
+
+    public Szafa() {
+        this.ubrania = new ArrayList<>();
+    }
 }
