d284ebd987620c7fbd9a913d4dc23562e627157a
Daniel Filipowicz <daniel04f@gmail.com>
2024-07-29 22:16:20 +0200

    -Added Szafa Object -Added dodajUbranie(), dodajUbranie(u1, u2) and dodajUbranie(u1, u2, u3, u4) callings

--- src/main/java/pl/kurs/test4/task1/Runner.java
+++ src/main/java/pl/kurs/test4/task1/Runner.java
@@ -6,5 +6,10 @@ public class Runner {
         Ubranie u2 = new Ubranie("Jeansy", Ubranie.Rozmiar.S, 120.0);
         Ubranie u3 = new Ubranie("Majtki", Ubranie.Rozmiar.XL, 55.0);
         Ubranie u4 = new Ubranie("Koszula", Ubranie.Rozmiar.M, 481.0);
+
+        Szafa szafa = new Szafa();
+        szafa.dodajUbranie();
+        szafa.dodajUbranie(u1, u2);
+        szafa.dodajUbranie(u1, u2, u3, u4);
     }
 }
