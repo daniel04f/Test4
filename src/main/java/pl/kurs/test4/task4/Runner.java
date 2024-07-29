0050677ec7c20f1a11276e85a13aad3e843cec7d
Daniel Filipowicz <daniel04f@gmail.com>
2024-07-29 22:45:42 +0200

    -Added Integer List -Added getListOfFiveBiggestElementsFromIntegersList method calling

--- src/main/java/pl/kurs/test4/task4/Runner.java
+++ src/main/java/pl/kurs/test4/task4/Runner.java
@@ -1,7 +1,12 @@
 package pl.kurs.test4.task4;
 
+import java.util.List;
+
 public class Runner {
     public static void main(String[] args) {
+        List<Integer> numbers = List.of(1, 8, 3, 17, -2, -1);
 
+        List<Integer> resultList = Service.getListOfFiveBiggestElementsFromIntegersList(numbers);
+        System.out.println("resultList = " + resultList);
     }
 }
