8fdb9e0ce4724da808642da204d7d5807a637143
Daniel Filipowicz <daniel04f@gmail.com>
2024-07-29 22:15:34 +0200

    -Added Ubranie u1, Ubranie u2, Ubranie u3, Ubranie u4

--- src/main/java/pl/kurs/test4/task1/Runner.java
+++ src/main/java/pl/kurs/test4/task1/Runner.java
@@ -2,6 +2,9 @@ package pl.kurs.test4.task1;
 
 public class Runner {
     public static void main(String[] args) {
-
+        Ubranie u1 = new Ubranie("Koszula", Ubranie.Rozmiar.L, 299.0);
+        Ubranie u2 = new Ubranie("Jeansy", Ubranie.Rozmiar.S, 120.0);
+        Ubranie u3 = new Ubranie("Majtki", Ubranie.Rozmiar.XL, 55.0);
+        Ubranie u4 = new Ubranie("Koszula", Ubranie.Rozmiar.M, 481.0);
     }
 }
