44a2c070c7ed5906c8edc064a1b2098bf351c62f
Daniel Filipowicz <daniel04f@gmail.com>
2024-07-29 22:10:34 +0200

    -Added class fields -Added enum Rozmiar -Added enum Kryterium

--- src/main/java/pl/kurs/test4/task1/Ubranie.java
+++ src/main/java/pl/kurs/test4/task1/Ubranie.java
@@ -1,4 +1,22 @@
 package pl.kurs.test4.task1;
 
 public class Ubranie {
+    private String nazwa;
+    private Rozmiar rozmiar;
+    private double cena;
+    private static Kryterium kryteriumSortowania = Kryterium.ROZMIAR;
+
+    public enum Rozmiar {
+        XS,
+        S,
+        M,
+        L,
+        XL,
+        XXL
+    }
+
+    public enum Kryterium {
+        ROZMIAR,
+        WARTOSC
+    }
 }
