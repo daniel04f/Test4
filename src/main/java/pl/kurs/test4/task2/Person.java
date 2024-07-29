8e2e26e41286af3c8928274be03fc21fe399f208
Daniel Filipowicz <daniel04f@gmail.com>
2024-07-29 22:24:49 +0200

    -Added equals() and hashCode() methods -Added toString method

--- src/main/java/pl/kurs/test4/task2/Person.java
+++ src/main/java/pl/kurs/test4/task2/Person.java
@@ -1,5 +1,7 @@
 package pl.kurs.test4.task2;
 
+import java.util.Objects;
+
 public class Person {
     private String firstName;
     private String lastName;
@@ -23,5 +25,28 @@ public class Person {
 
     public boolean isWoman() {
         return firstName.endsWith("a");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && Objects.equals(firstName, person.firstName) && Objects.equals(lastName, person.lastName) && Objects.equals(city, person.city);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, city, age);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", city='" + city + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
