9d781342c55823ee06cbb2d4121939f1972c1fce
Daniel Filipowicz <daniel04f@gmail.com>
2024-07-29 22:27:27 +0200

    -Added getTheOldestWomen method

--- src/main/java/pl/kurs/test4/task2/PersonService.java
+++ src/main/java/pl/kurs/test4/task2/PersonService.java
@@ -1,4 +1,15 @@
 package pl.kurs.test4.task2;
 
+import java.util.*;
+
 public class PersonService {
+    public static Person getTheOldestWomen(List<Person> people) throws NoWomenException {
+        return Optional.ofNullable(people)
+                .orElseGet(Collections::emptyList)
+                .stream()
+                .filter(Objects::nonNull)
+                .filter(Person::isWoman)
+                .max(Comparator.comparingInt(Person::getAge))
+                .orElseThrow(() -> new NoWomenException("Nie znaleziono żadnej kobiety na liście"));
+    }
 }
