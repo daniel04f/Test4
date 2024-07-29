b1d6e960e222749ec3ee2ac74314aad91bd8f799
Daniel Filipowicz <daniel04f@gmail.com>
2024-07-29 22:37:41 +0200

    -Added askForName method -Added getNameLength method

--- src/main/java/pl/kurs/test4/task3/UserService.java
+++ src/main/java/pl/kurs/test4/task3/UserService.java
@@ -1,4 +1,19 @@
 package pl.kurs.test4.task3;
 
+import java.util.Optional;
+import java.util.Scanner;
+
 public class UserService {
+    public static int askForName(Scanner scanner) {
+        System.out.print("Podaj swoje imię: ");
+        String name = scanner.nextLine();
+        return getNameLength(name);
+    }
+
+    private static int getNameLength(String name) {
+        return Optional.ofNullable(name)
+                .filter(x -> !x.isEmpty())
+                .map(String::length)
+                .orElse(0);
+    }
 }
